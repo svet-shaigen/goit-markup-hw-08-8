1325483f739595f28042d8349858175dd9f44ff7
svet-shaigen <110323981+svet-shaigen@users.noreply.github.com>
2022-12-17 14:15:11 +0200

    Update scss file

--- js/menu.js
+++ js/menu.js
@@ -9,6 +9,7 @@
     refs.openMenuBtn.addEventListener('click', toggleMenu);
     refs.closeMenuBtn.addEventListener('click', toggleMenu);
     refs.menuList.addEventListener('click', removeMenu);
+    
     function toggleMenu() {
       refs.menu.classList.toggle('is-hidden');
       refs.body.classList.toggle('no-scroll');
--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,19 @@
       openModalBtn: document.querySelector("[data-modal-open]"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
       modal: document.querySelector("[data-modal]"),
+      modalList: document.querySelector('.menu-list'),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
-  
+    refs.modalList.addEventListener('click', removeModal);
+
     function toggleModal() {
       refs.modal.classList.toggle("is-hidden");
+      refs.body.classList.toggle('no-scroll');
+    }
+    function removeMenu() {
+      refs.modal.classList.add('is-hidden');
+      refs.body.classList.remove('no-scroll');
     }
   })();
